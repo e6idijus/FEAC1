0e54aa1a6e69e286500ea0c08589f232c5a38103
Egidijus <egidijusm@gmail.com>
2024-06-26 17:26:50 +0300

    Add styles to CategoryCard component

--- Home Service App/Frontend/src/components/home/CategoryCard.jsx
+++ Home Service App/Frontend/src/components/home/CategoryCard.jsx
@@ -1,11 +1,16 @@
+import styles from "./CategoryList.module.scss";
+
 const CategoryCard = ({ category }) => {
   const { name, icon } = category;
   const Icon = icon;
 
   return (
-    <div>
-      <Icon fontSize={48} />
-      <p>{name}</p>
+    <div className={styles.card}>
+      <Icon
+        fontSize={48}
+        color={category.color}
+      />
+      <p className={styles.name}>{name}</p>
     </div>
   );
 };
