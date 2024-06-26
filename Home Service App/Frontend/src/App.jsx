b6ae0f559f74cd3491c8ad328627f4ab870440e2
Egidijus <egidijusm@gmail.com>
2024-06-26 15:57:09 +0300

    Add Hero component

--- Home Service App/Frontend/src/App.jsx
+++ Home Service App/Frontend/src/App.jsx
@@ -1,9 +1,11 @@
+import Hero from "./components/home/Hero";
 import Topbar from "./components/layout/Topbar";
 
 function App() {
   return (
     <>
       <Topbar />
+      <Hero />
     </>
   );
 }
---
+++ Home Service App/Frontend/src/components/home/Hero.jsx
@@ -0,0 +1,21 @@
+import Button from "../common/Button";
+
+const Hero = () => {
+  return (
+    <div>
+      <h1>
+        Find Home Service/Repair
+        <br />
+        Near You
+      </h1>
+      <p>Explore Best Home Service & Repair near you</p>
+      <div>
+        <Button rounded>
+          <div>Search</div>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
