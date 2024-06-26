627b47cd51e20ef64f77ab006077e538ca49093b
Egidijus <egidijusm@gmail.com>
2024-06-26 16:02:36 +0300

    Add styles to Hero component

--- Home Service App/Frontend/src/components/home/Hero.jsx
+++ Home Service App/Frontend/src/components/home/Hero.jsx
@@ -1,14 +1,17 @@
 import Button from "../common/Button";
+import styles from "./hero.module.scss";
 
 const Hero = () => {
   return (
-    <div>
-      <h1>
-        Find Home Service/Repair
+    <div className={styles.hero}>
+      <h1 className={styles.title}>
+        Find Home <span className={styles.primary}>Service/Repair</span>
         <br />
         Near You
       </h1>
-      <p>Explore Best Home Service & Repair near you</p>
+      <p className={styles.subtitle}>
+        Explore Best Home Service & Repair near you
+      </p>
       <div>
         <Button rounded>
           <div>Search</div>
