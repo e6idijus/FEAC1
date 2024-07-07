cd67423089af3807cb45a8bd32d9a87f7e4b2b7e
Egidijus <egidijusm@gmail.com>
2024-07-07 15:52:24 +0300

    Add router to App

--- Home Service App/Frontend/src/App.jsx
+++ Home Service App/Frontend/src/App.jsx
@@ -1,15 +1,44 @@
-import CategoryList from "./components/home/CategoryList";
-import Hero from "./components/home/Hero";
-import Topbar from "./components/layout/Topbar";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import AboutUs from "./pages/AboutUs";
+import { ROUTES } from "./router/consts";
+import RootLayout from "./components/layout/RootLayout";
+import ErrorPage from "./pages/ErrorPage";
+import Login from "./pages/Login";
+import SearchCategory from "./pages/SearchCategory";
 
-function App() {
-  return (
-    <>
-      <Topbar />
-      <Hero />
-      <CategoryList />
-    </>
-  );
-}
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: ROUTES.HOME,
+        element: <Home />,
+      },
+      {
+        path: ROUTES.SERVICES,
+        element: <Services />,
+      },
+      {
+        path: ROUTES.ABOUT_US,
+        element: <AboutUs />,
+      },
+      {
+        path: ROUTES.LOGIN,
+        element: <Login />,
+      },
+      {
+        path: ROUTES.SEARCH_CATEGORY,
+        element: <SearchCategory />,
+      },
+    ],
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
