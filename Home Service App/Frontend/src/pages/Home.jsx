ea3008f6067a56b266826c72e36d3aa1548da951
Egidijus <egidijusm@gmail.com>
2024-07-13 15:09:34 +0300

    Add absolute imports

--- Home Service App/Frontend/src/pages/Home.jsx
+++ Home Service App/Frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
-import Topbar from "../components/layout/Topbar";
-import Hero from "../components/home/Hero";
-import CategoryList from "../components/home/CategoryList";
+import Topbar from "@/components/layout/Topbar";
+import Hero from "@/components/home/Hero";
+import CategoryList from "@/components/home/CategoryList";
 
 const Home = () => {
   return (
