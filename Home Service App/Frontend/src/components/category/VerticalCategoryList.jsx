acb7f40d502e2be3f0f8de9bfe163f604e19cbd7
Egidijus <egidijusm@gmail.com>
2024-07-13 18:46:55 +0300

    Add style to VerticalCategoryList

--- Home Service App/Frontend/src/components/category/VerticalCategoryList.jsx
+++ Home Service App/Frontend/src/components/category/VerticalCategoryList.jsx
@@ -1,10 +1,11 @@
-import { categories } from "@/consts/category";
+import { categories } from "../../consts/category";
 import CategoryCard from "./CategoryCard";
+import styles from "./VerticalCategoryList.module.scss";
 
 const VerticalCategoryList = () => {
   return (
     <div>
-      <h2>Categories</h2>
+      <h2 className={styles.title}>Categories</h2>
       {categories.map((category) => (
         <CategoryCard
           key={category.name}
