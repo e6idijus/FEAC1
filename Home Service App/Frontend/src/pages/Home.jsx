e3f88b865df207362c68c6ae9b747c366615fd79
Egidijus <egidijusm@gmail.com>
2024-07-13 19:55:16 +0300

    Add style to Home component

--- Home Service App/Frontend/src/pages/Home.jsx
+++ Home Service App/Frontend/src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import Hero from "@/components/common/Hero";
 import CategoryList from "@/components/home/CategoryList";
 import BusinessList from "@/components/home/BusinessList";
+import styles from "./Home.module.scss";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <CategoryList />
-      <h2>Popular businesses</h2>
+      <h2 className={styles.title}>Popular businesses</h2>
       <BusinessList />
     </>
   );
