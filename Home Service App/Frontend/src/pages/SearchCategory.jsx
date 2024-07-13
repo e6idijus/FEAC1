d90e83dbff99405ef4b93cf5715e41613bd6163a
Egidijus <egidijusm@gmail.com>
2024-07-13 18:24:49 +0300

    Add VerticalCategoryList component to SearchCategory

--- Home Service App/Frontend/src/pages/SearchCategory.jsx
+++ Home Service App/Frontend/src/pages/SearchCategory.jsx
@@ -1,9 +1,16 @@
 import { useParams } from "react-router-dom";
+import VerticalCategoryList from "@/components/category/VerticalCategoryList";
 
 const SearchCategory = () => {
   const { category } = useParams();
 
-  return <div>It's {category}</div>;
+  return (
+    <div>
+      <div>
+        <VerticalCategoryList />
+      </div>
+    </div>
+  );
 };
 
 export default SearchCategory;
