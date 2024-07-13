09618bde0274acb3cd47b6f3ba49a0447d75f2f5
Egidijus <egidijusm@gmail.com>
2024-07-13 19:34:02 +0300

    Add BussinesList to Home page

--- Home Service App/Frontend/src/pages/Home.jsx
+++ Home Service App/Frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
-import Topbar from "@/components/layout/Topbar";
 import Hero from "@/components/common/Hero";
 import CategoryList from "@/components/home/CategoryList";
+import BusinessList from "@/components/home/BusinessList";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <CategoryList />
+      <h2>Popular businesses</h2>
+      <BusinessList />
     </>
   );
 };
