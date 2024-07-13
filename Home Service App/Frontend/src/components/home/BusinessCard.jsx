103cdf70e7fb66f414d17167c69ba0c19e0bccde
Egidijus <egidijusm@gmail.com>
2024-07-13 19:38:01 +0300

    Add style to BusinessCard component

--- Home Service App/Frontend/src/components/home/BusinessCard.jsx
+++ Home Service App/Frontend/src/components/home/BusinessCard.jsx
@@ -1,19 +1,21 @@
 import Button from "../common/Button";
+import styles from "./BusinessCard.module.scss";
 
 const BusinessCard = ({ business }) => {
   return (
-    <div>
+    <div className={styles.card}>
       {business.images.length && (
         <img
           src={business.images[0].url}
           alt={business.name}
+          className={styles.image}
         />
       )}
-      <div>
-        <span>{business.category}</span>
-        <h3>{business.name}</h3>
-        <p>{business.contactPerson}</p>
-        <p>{business.address}</p>
+      <div className={styles.infoContainer}>
+        <span className={styles.chip}>{business.category}</span>
+        <h3 className={styles.name}>{business.name}</h3>
+        <p className={styles.contactPerson}>{business.contactPerson}</p>
+        <p className={styles.address}>{business.address}</p>
         <Button>Book now</Button>
       </div>
     </div>
