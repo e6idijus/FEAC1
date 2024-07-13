87c239043c4e18eaafa53f6cad64438a7dd2bc85
Egidijus <egidijusm@gmail.com>
2024-07-13 18:39:11 +0300

    Add routing to CategoryCard component

--- Home Service App/Frontend/src/components/category/CategoryCard.jsx
+++ Home Service App/Frontend/src/components/category/CategoryCard.jsx
@@ -1,15 +1,26 @@
+import { useNavigate, generatePath, useParams } from "react-router-dom";
+import classNames from "classnames";
 import UrlIcon from "../common/UrlIcon";
+import { ROUTES } from "@/router/consts";
+import styles from "./CategoryCard.module.scss";
 
 const CategoryCard = ({ category }) => {
+  const params = useParams();
   const { name } = category;
+  const navigate = useNavigate();
+
+  const categoryPath = generatePath(ROUTES.SEARCH_CATEGORY, { category: name });
 
   return (
-    <div>
+    <div
+      className={styles.card}
+      onClick={() => navigate(categoryPath)}
+    >
       <UrlIcon
         url={category.url}
         style={{ width: 48, height: 48, backgroundColor: category.color }}
       />
-      <p>{name}</p>
+      <p className={styles.name}>{name}</p>
     </div>
   );
 };
