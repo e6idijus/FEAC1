0a396b73c9e842b0adaa09c8b4a01ff78bef6860
Egidijus <egidijusm@gmail.com>
2024-07-13 19:31:56 +0300

    Move Hero.jsx and Hero,module.scss to 'common' directory Also update imports

--- Home Service App/Frontend/src/pages/Home.jsx
+++ Home Service App/Frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import Topbar from "@/components/layout/Topbar";
-import Hero from "@/components/home/Hero";
+import Hero from "@/components/common/Hero";
 import CategoryList from "@/components/home/CategoryList";
 
 const Home = () => {
