2504e80fcd0ba9957fa8170d58dd29e92e3edc35
Egidijus <egidijusm@gmail.com>
2024-07-14 18:08:22 +0300

    Add form to Login component

--- Home Service App/Frontend/src/pages/Login.jsx
+++ Home Service App/Frontend/src/pages/Login.jsx
@@ -1,5 +1,28 @@
+import Input from "@/components/common/Input";
+import Button from "@/components/common/Button";
+
 const Login = () => {
-  return <h1>Login</h1>;
+  return (
+    <div>
+      <form>
+        <h2>Login</h2>
+        <Input
+          type="email"
+          placeholder="Email"
+          required
+        />
+        <Input
+          type="password"
+          placeholder="Password"
+          required
+        />
+        <Button type="submit">Log in</Button>
+        <div>
+          <a href="#">Don't have an account? Sign up</a>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
