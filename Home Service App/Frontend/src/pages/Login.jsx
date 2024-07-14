ea194fa1219fc9b7e0697e9108090e281d34eada
Egidijus <egidijusm@gmail.com>
2024-07-14 18:22:48 +0300

    Add style to Login component

--- Home Service App/Frontend/src/pages/Login.jsx
+++ Home Service App/Frontend/src/pages/Login.jsx
@@ -1,23 +1,26 @@
 import Input from "@/components/common/Input";
+import styles from "./Login.module.scss";
 import Button from "@/components/common/Button";
 
 const Login = () => {
   return (
-    <div>
-      <form>
-        <h2>Login</h2>
+    <div className={styles.container}>
+      <form className={styles.form}>
+        <h2 className={styles.title}>Login</h2>
         <Input
           type="email"
           placeholder="Email"
           required
+          className={styles.input}
         />
         <Input
           type="password"
           placeholder="Password"
           required
+          className={styles.input}
         />
         <Button type="submit">Log in</Button>
-        <div>
+        <div className={styles.link}>
           <a href="#">Don't have an account? Sign up</a>
         </div>
       </form>
