af4f6f4f113881fe8d2734d36963e90f701821a9
Egidijus <egidijusm@gmail.com>
2024-07-14 18:24:19 +0300

    Save Login form data using useState

--- Home Service App/Frontend/src/pages/Login.jsx
+++ Home Service App/Frontend/src/pages/Login.jsx
@@ -1,21 +1,38 @@
 import Input from "@/components/common/Input";
 import styles from "./Login.module.scss";
 import Button from "@/components/common/Button";
+import { useState } from "react";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("email: ", email);
+    console.log("passsword: ", password);
+  };
+
   return (
     <div className={styles.container}>
-      <form className={styles.form}>
+      <form
+        onSubmit={handleSubmit}
+        className={styles.form}
+      >
         <h2 className={styles.title}>Login</h2>
         <Input
           type="email"
           placeholder="Email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
           required
           className={styles.input}
         />
         <Input
           type="password"
           placeholder="Password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
           required
           className={styles.input}
         />
