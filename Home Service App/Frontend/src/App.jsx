026af0ddbb8804b84d8448e378779d52fe68a709
Egidijus <egidijusm@gmail.com>
2024-07-21 09:39:23 +0300

    Add AuthLayout to router

--- Home Service App/Frontend/src/App.jsx
+++ Home Service App/Frontend/src/App.jsx
@@ -7,6 +7,7 @@ import RootLayout from "./components/layout/RootLayout";
 import ErrorPage from "./pages/ErrorPage";
 import Login from "./pages/Login";
 import SearchCategory from "./pages/SearchCategory";
+import AuthLayout from "./components/layout/AuthLayout";
 import Register from "./pages/Register";
 import { UserProvider } from "./context/UserContext";
 
@@ -28,16 +29,22 @@ const router = createBrowserRouter([
         element: <AboutUs />,
       },
       {
+        path: ROUTES.SEARCH_CATEGORY,
+        element: <SearchCategory />,
+      },
+    ],
+  },
+  {
+    element: <AuthLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
         path: ROUTES.LOGIN,
         element: <Login />,
       },
       {
         path: ROUTES.REGISTER,
         element: <Register />,
-      },
-      {
-        path: ROUTES.SEARCH_CATEGORY,
-        element: <SearchCategory />,
       },
     ],
   },
